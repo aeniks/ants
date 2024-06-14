6772117df49079d7619b5e92edc2bc0fe60724ca
aeniks <hello@aeniks.com>
2024-06-14 11:54:01 +0200

    Steinway

--- installers/basic_apps.sh
+++ installers/basic_apps.sh
@@ -5,19 +5,24 @@ sudo apt update; sudo apt upgrade -y;
 ##################
 sudo apt -y install \
 net-tools \
-ifconfig \
 lolcat \
 fortunes \
 fortune-mod \
-cowsay \; 
+cowsay \
+neofetch \
+mc \
+btop \
+
+
+; 
 
 
 ##############
 echo;echo;
-echo "${cows[$((RANDOM%${#cows[@]}))]}"
+#echo "${cows[$((RANDOM%${#cows[@]}))]}"
 fortunes=$(ls /usr/share/games/fortunes)
 cows=($(ls /usr/share/cowsay/cows|sed s/.cow//g)); 
-alias qqqq='/usr/games/fortune|/usr/games/cowthink -f ${cows[$((RANDOM%${#cows[@]}))]}|lolcat;'
+#alias qqqq='/usr/games/fortune|/usr/games/cowthink -f ${cows[$((RANDOM%${#cows[@]}))]}|lolcat;'
 sudo cp /ants/sh/ssss.sh /bin/ssss;
-sudo chown $SUDO_USER:$USER /bin/ssss 2>/dev/null;
+sudo chown $SUDO_USER:$USER /bin/ssss;
 echo "ok";
