dc7005f9c0e68d30569c837c1642d6e9c249d26e
aeniks <hello@aeniks.com>
2024-06-14 11:51:15 +0200

    bricklayers

--- installers/basic_apps.sh
+++ installers/basic_apps.sh
@@ -1,9 +1,21 @@
 #/bin/bash
 # Installs basic apps and functions
 tput dim; 
-sudo apt update; sudo apt -qqy install net-tools ifconfig lolcat fortunes fortune-mod cowsay 2>/dev/null; echo;echo;
-#echo "${cows[$((RANDOM%${#cows[@]}))]}"
-#fortunes=$(ls /usr/share/games/fortunes)
+sudo apt update; sudo apt upgrade -y;
+##################
+sudo apt -y install \
+net-tools \
+ifconfig \
+lolcat \
+fortunes \
+fortune-mod \
+cowsay \; 
+
+
+##############
+echo;echo;
+echo "${cows[$((RANDOM%${#cows[@]}))]}"
+fortunes=$(ls /usr/share/games/fortunes)
 cows=($(ls /usr/share/cowsay/cows|sed s/.cow//g)); 
 alias qqqq='/usr/games/fortune|/usr/games/cowthink -f ${cows[$((RANDOM%${#cows[@]}))]}|lolcat;'
 sudo cp /ants/sh/ssss.sh /bin/ssss;
