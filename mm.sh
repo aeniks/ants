64bfffcf49933a8792ded932906fdfa053d2a150
aeniks <hello@aeniks.com>
2024-06-19 05:04:32 +0200

    Malaysians

--- mm.sh
+++ mm.sh
@@ -3,12 +3,11 @@
 menu() {
 dim='\e[2m'; green='\e[32m'; re='\e[0m'; hide='\e[8m';
 line="$dim---------------------------"; dots="$dim.............."
-
 unset opt choices i nm sel ACTIONS err cole; opt=($(ls -pfA1|grep -v /|head -n9))
 for i in $(seq $((LINES-2))); do echo; done; echo -ne "\e[2f\e[0J\e[2f"
 ## MENU FUNCTION ####
 echo -ne " $line\n ------$re BASH$dim-"$re"SELECTOR$dim ------\n $line$re
- ["$dim"1"$re"-"$dim"9"$re"] select/unselect ["$dim"a"$re"] select/unselect all
+ ["$dim"1"$re"-"$dim"9"$re"] select/unselect ["$dim"a"$re"] select/unselect all\
  ["$dim"ENTER"$re"] confirm ["$dim"q"$re"] exit \n $line$re\n\e[s";
 MENU() {
 cole=$(stty size|tr " " "\n"|tail -n1)
@@ -27,10 +26,6 @@ if [ $sel == a ]; then if [ -z $choices ]; then unset choices; for i in ${!opt[@
 else unset choices; fi; fi; 
 if [ $sel == q ]; then echo -e "\n\e[?25h\e[0m"; echo -e "\n\n\n\n\n\n gg\n\n"; return 0; fi; 
 done;
-##
-
-##
-##
 ## CONFIRMATION #####
 for i in ${!opt[@]}; do if [[ ${choices[$i]} ]]; 
 then echo -e "$re Option $((i+1)) ($dim${opt[i]}$re) selected"; fi; done;
