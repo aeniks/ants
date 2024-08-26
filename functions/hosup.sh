4d1a77d143baa05f24407ed395599e719a00bd94
aeniks <hello@aeniks.com>
2024-08-26 21:53:06 +0200

    2024-08-26_21_53

--- functions/hosup.sh
+++ functions/hosup.sh
@@ -1,3 +1,5 @@
 hosup() {
+arp -n|grep -v "incomplete"
+
 host="192.168.0.1"; for i in $(seq -w 00 20); do ping ${host}${i} -W .2 -qACc1 &>/dev/null && echo -e "host ${dim}${host}\e[1;4m$i\e[0m is$green up"$re; done
 }
