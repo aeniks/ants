9345b08eab08db12e6b52b4ceacda5f858d576dc
aeniks <hello@aeniks.com>
2024-08-30 18:12:31 +0200

    2024-08-30_18_12

--- sh/crons/rsync_lenovo_to_12ants.sh
+++ sh/crons/rsync_lenovo_to_12ants.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
-## uu
-echo; 
-echo "-----------------------------"; 
-date;
-rsync --mkpath --info=name1 -hvaz --exclude=".thumbnails" --exclude=".git" --max-size=999mb \
-"/home/aa/Pictures" "/home/aa/Public" "/home/aa/Documents" "/home/aa/Music" \
-aaaa@ants.ftp.sh:/media/sata1/backups/leonel/;
-echo "aaaa@ants.ftp.sh:/media/sata1/backups/leonel/"
-echo "-----------------------------";
+## uu ##############
+####################
+####################
+end='aaaa@ants.ftp.sh:/media/sata1/backups/leonel/' ##################
+####################
+include='
+/home/aa/Pictures 
+/home/aa/Public 
+/home/aa/Documents 
+/home/aa/Music
+' ##################
+####################
+exclude='
+--exclude=".thumbnails" 
+--exclude=".git" 
+' ##################
+####################
+specs='
+-a
+--info=name1 
+--max-size=999mb 
+' ##################
+####################
+##
+##
+## test run ########
+rsync --dry-run $specs $include $exclude $end|grep "" &>/dev/null &&
+(
+## go ##############
+
+echo -e "-----------------------------"; date; 
+echo -e "----------------------------- \n$end"; 
+echo -e "-----------------------------";
+rsync $specs $include $exclude $end; 
+echo -e "----------------------------- \n$end"; 
+echo -e "-----------------------------"; date;
+echo -e "----------------------------- \n\n ";
+)
+####################
+####################
