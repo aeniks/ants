68cc739147fedfff706604032e05afbc7cc99d9d
aeniks <hello@aeniks.com>
2024-09-01 09:06:31 +0200

    2024-09-01_09_06

--- functions/tttttard.sh
+++ functions/tttttard.sh
@@ -1,6 +1,7 @@
 tards() {
-for i in {1..99}; do echo -e "\e[H\e[48m"; 
+for i in {1..99}; do 
+echo -e "\[25?l\e[H\e[48m"; 
 pic=${1}; [ $1 ]||local pic="$ants/media/tard.jpg";
-jp2a $pic --color-depth=8 --chars="  $(echo -e "$SRANDOM$SRANDOM$SRANDOM$SRANDOM"|tr -s "0-9" " -_")"; 
-sleep .1; done
+jp2a $pic --color-depth=8 --chars="  $(echo -e "$SRANDOM$SRANDOM$SRANDOM$SRANDOM")"|pv --quiet --rate-limit 2222; 
+done; echo -e "\[25?h"
 }
