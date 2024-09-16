be16313fde2525000dbbfac870c2b18ef4a8ece9
aeniks <aeniks>
2024-09-16 16:04:48 +0200

    2024-09-16_16_04

--- sh/crons/issue_figz.sh
+++ sh/crons/issue_figz.sh
@@ -5,4 +5,4 @@ figlet -f "$f1" "h_e_l_l_o" | tee -a /home/aa/issues.net  > "/etc/issue.net";
 figlet -f "$f2" "h_e_l_l_o" | tee -a /home/aa/issues > "/etc/issue"; 
 sudo chown aa: /home/aa/issues*; 
 sudo chmod 775 /home/aa/issues*; 
-rsync /home/aa/issues.net /home/aa/issues aa@192.168.0.100:/home/aa/
+#rsync /home/aa/issues.net /home/aa/issues aa@192.168.0.100:/home/aa/
