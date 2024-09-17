f61154399e801037e8f084dc3439de04e0c5ffb0
aeniks <aeniks>
2024-09-17 22:51:55 +0200

    2024-09-17_22_51

--- functions/zanc.sh
+++ functions/zanc.sh
@@ -2,24 +2,39 @@
 ## zanc ##############
 zanc() {
 ###################### > colors
+touch 'ants.ftp.sh'; touch '192.168.0.10'; 
 re="$(printf "\e[0m")"; rev="$(printf "\e[7m")"; p1=; p2=;
 cyan="$(printf "\e[36m")"; green="$(printf "\e[32m")"; 
 ###################### > other variables
-to='192.168.0.100'; path='~/'; port='22'; include="$PWD"; usr="$USER"; 
+to='ants.ftp.sh'; path='~/'; port='22'; include="$PWD"; usr="$USER"; 
 exclude='--exclude="*.thumbnails*" --exclude="*/.git*" --exclude="*cache*" '; 
 specs='--info=name1 --max-size=999mb --prune-empty-dirs -a'; log="$HOME/log.log"
 ###################### > read 
 printf "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\e[18A
 $green  --$re RSYNC$green ----------$re \n"; 
-read -rep " $green --$re include: " -i "$include" "include";
-read -rep " $green --$re exclude: " -i "$exclude" "exclude";
-read -rep " $green --$re specs: " -i "$specs" "specs";
-read -rep " $green --$re to: " -i "$to" "to";
-read -rep " $green --$re user: " -i "$usr" "usr";
-read -rep " $green --$re remote-path: " -i "$path" "path";
-read -rep " $green --$re port: " -i "$port" "port";
-read -rep " $green --$re log: " -i "$log" "log";
-read -rep " $green --$re save sync-file: " -i "$PWD/rsync_to_$to.sh" "rsf";
+printf " $green --$re include: \n" 
+printf " $green --$re exclude: \n" 
+printf " $green --$re specs: \n" 
+printf " $green --$re to: \n" 
+printf " $green --$re user: \n" 
+printf " $green --$re remote-path: \n" 
+printf " $green --$re port: \n" 
+printf " $green --$re log: \n" 
+printf " $green --$re save sync-file: \n" 
+printf  "$green  ----------------$re\n"; 
+printf "\e[12A";
+####################
+printf "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\e[18A
+$green  --$re RSYNC$green ----------$re \n"; 
+read -rep " $green --$re include: " -i "$include" "include"; 
+read -rep " $green --$re exclude: " -i "$exclude" "exclude"; 
+read -rep " $green --$re specs: " -i "$specs" "specs"; 
+read -rep " $green --$re to: " -i "$to" "to"; 
+read -rep " $green --$re user: " -i "$usr" "usr"; 
+read -rep " $green --$re remote-path: " -i "$path" "path"; 
+read -rep " $green --$re port: " -i "$port" "port"; 
+read -rep " $green --$re log: " -i "$log" "log"; 
+read -rep " $green --$re save sync-file: " -i "$PWD/rsync_to_$to.sh" "rsf"; 
 printf  "$green  ----------------$re\n"; 
 read -n1 -rep " $green --$re OK ["$green"Y"$re"/n] " "kk"; 
 [ "$kk" ]&& return; if [ $port -gt 800 ]; then p1="-e"; p2=""'"'"ssh -p $port"'"'""; fi; 
@@ -33,6 +48,6 @@ rsync '"$specs $exclude $include $p1 $p2 $to:$path "'
 #  -----------------------------  # 
 ##  -- logs to: '$log' 
 \n'|tee "$rsf"; chmod 775 "$rsf"; printf "$green  --$re zync-file is at:$green $rsf $re\n\n"; 
-
+rm 'ants.ftp.sh'; rm '192.168.0.10'; 
 ## test run ########
 } 
--- installers/grub.sh
+++ installers/grub.sh
@@ -12,16 +12,16 @@ sudo cp $ants/media/tard.jpg /boot/grub/tard.jpg; chmod 775 /boot/grub/tard.jpg;
 echo '
 GRUB_BACKGROUND="/boot/grub/tard.jpg"
 GRUB_GFXMODE=auto
-GRUB_TERMINAL_OUTPUT="gfxterm"
+GRUB_TERMINAL_OUTPUT="console"
 GRUB_DEFAULT=saved
 GRUB_SAVEDEFAULT=true
 GRUB_TIMEOUT_STYLE=menu
 GRUB_TIMEOUT=4
-GRUB_DISTRIBUTOR=`lsb_release -dcs 2> /dev/null || echo Debian`
+GRUB_DISTRIBUTOR=`lsb_release -dcs 2>/dev/null || echo Debian`
 GRUB_DISABLE_OS_PROBER=false
 GRUB_HIDDEN_TIMEOUT=0
-GRUB_CMDLINE_LINUX="gfxterm"
-GRUB_CMDLINE_LINUX_DEFAULT="gfxterm"
+GRUB_CMDLINE_LINUX="console"
+GRUB_CMDLINE_LINUX_DEFAULT="console"
 GRUB_GFXPAYLOAD_LINUX=keep
 ' > /etc/default/grub;
 sudo update-grub ;
