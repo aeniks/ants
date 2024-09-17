b2da1c627b6fb6b12799fc1dbc81eb37705ef362
aeniks <aeniks>
2024-09-17 03:46:56 +0200

    2024-09-17_03_46

--- sh/q/lf_install.sh
+++ sh/q/lf_install.sh
@@ -4,7 +4,7 @@ lf_install() {
 mkdir -p ~/tmp/lf_bu_$(date +%F)
 mv ~/.config/lf/* ~/tmp/lf_bu_$(date +%F)/
 ln $ants/sh/config/lf/lfrc ~/.config/lf/ --symbolic
-ln $ants/sh/config/lf/preview ~/.config/lf/ --symbolic
+ln $ants/sh/config/lf/previewer ~/.config/lf/ --symbolic
 ln $ants/sh/config/lf/icons ~/.config/lf/ --symbolic
 ln $ants/sh/config/lf/opener ~/.config/lf/ --symbolic
 ln $ants/sh/config/lf/colors ~/.config/lf/ --symbolic
