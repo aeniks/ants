5774f4b7525a658747e6a266085f94dfa8d7662d
aeniks <aeniks>
2024-09-18 06:04:30 +0200

    2024-09-18_06_04

--- sh/q/lf_install.sh
+++ sh/q/lf_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # choose files to gist (-p) to public
 lf_install() { 
-sudo apt install -y lf chafa jp2a exiftool caca-utils batcat 2>/dev/null; 
 mkdir -p ~/.config/lf 
+mkdir -p ~/.cache/lf 
 mkdir -p ~/tmp/lf_bu_$(date +%F)
 mv ~/.config/lf/* ~/tmp/lf_bu_$(date +%F)/
 ln $ants/sh/config/lf/lfrc ~/.config/lf/ --symbolic
@@ -11,6 +11,7 @@ ln $ants/sh/config/lf/icons ~/.config/lf/ --symbolic
 ln $ants/sh/config/lf/opener ~/.config/lf/ --symbolic
 ln $ants/sh/config/lf/colors ~/.config/lf/ --symbolic
 ln $ants/sh/config/lf/lf_m ~/.config/lf/ --symbolic
+sudo apt install -y ffmpeg-thumbnailer fontforge lf chafa jp2a exiftool caca-utils bat 2>/dev/null; 
 ####
 ####
 }
