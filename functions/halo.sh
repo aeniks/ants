539be456310310241faf4f8989aac8769da6ffb5
aeniks <aeniks>
2024-09-18 08:07:27 +0200

    2024-09-18_08_07

--- functions/halo.sh
+++ functions/halo.sh
@@ -3,10 +3,11 @@
 halo() {
 printf "\e[?25l\e[$((LINES/6))H";
 for i in {1..111}; do printf "\e[38;5;${i}m\n\n\n\n"; 
-figlet -f DOS_Rebel "hello!" -w $COLUMNS -c; printf "\e[14A"; read -n1 -t.1 -res x; 
+figlet -f DOS_Rebel -c -w $COLUMNS "hello!"; printf "\e[14A"; read -n1 -t.1 -res x; 
 [ "$x" ]&&break; done; 
 for i in {111..222}; do printf "\e[38;5;${i}m\n\n\n\n"; 
-figlet -f DOS_Rebel "hello!" -w $COLUMNS -c; printf "\e[14A"; read -n1 -t.1 -res x; 
+figlet -f DOS_Rebel -c -w $((COLUMNS - RANDOM%6)) "hello!"; 
+printf "\e[14A"; read -n1 -t.1 -res x; 
 printf "\e[$((RANDOM%6))A\e[$((RANDOM%2))B"
 [ "$x" ]&&break; done; 
 printf "\e[?25h\e[8B\n\n\n\n\n\n\e[$((COLUMNS/2-16))G";
