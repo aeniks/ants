9d77895aa860c962432ba98ff2d39568e3ea04bf
aeniks <aeniks>
2024-11-03 04:46:14 +0100

    2024-11-03_04_46

---
+++ functions/lo.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+ 
+lo() {
+while true; do 
+ff2=$(figlist|shuf -n1); 
+for i in $(seq $((LINES))); do echo; sleep .02; done; printf "\e[$((LINES/2+12))A";
+tput civis; 
+figlet -c -w ${COLUMNS} -f "$ff2" "$(printf "\e[96m"; date +%a' '%d.%m%n%T)"|batcat -ppfl zig|\
+pv --rate-limit 666 --quiet;
+printf "\e7\e[2H\e[0m\e[2m  $(date -R)\e[0m   ||  \e[2m  $ff2\e[0m\e8"; 
+printf "\e[2m\n\n\n\n\e[0m  ······\n\e[2m"; fortune; printf "\e[0m  ······\n";
+sleep 22; tput cnorm; 
+done; 
+} 
+
+
+
+# lolo() { 
+# while true; do ff=$(figlist|shuf -n1); 
+# printf "\n\n$ff\n\n"; 
+# figlet -c -w ${COLUMNS} -f "$ff" "_Hello"|batcat -ppfl zig; 
+# printf "\n\n"; sleep 6; 
+# ff=$(figlist|shuf -n1); 
+# printf "\n\n$ff\n\n"; figlet -c -w ${COLUMNS} -f "$ff" "$(date +%X)"|batcat -ppfl zig; 
+# printf "\n\n"; 
+# fortune; done; 
+# }; 
+# 
+
+
+
+
+
+
